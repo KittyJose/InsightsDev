7677a986a11f812e591014da780deb92730beb12
Kitty Jose <54106287+KittyJose@users.noreply.github.com>
2021-01-13 12:17:32 +0000

    cdwew

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const DBConnect = require("./src/connect");
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-module.exports = (json) => {
+/*module.exports = (json) => {
     let config = {server : "https://127.0.0.1:6363", key : password, user: "admin", db:"Doc"}
     DBConnect(config, json)
 
     //return `url: ${url}, password: ${password}, json: ${json}`;
+}*/
+
+
+
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const jsonInp = core.getInput('json');
+  console.log('reached insight repo')
+  console.log(`recieved this from abother repo action ${jsonInp}!`);
+} catch (error) {
+  core.setFailed(error.message);
 }
